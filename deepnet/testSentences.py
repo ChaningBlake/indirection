fixed off by one bug where sentence at index zero never was assigned

deepnet/testSentences.py
@@ -1,6 +1,12 @@
 from sentenceSets import sentenceSets
-#[train_set,test_set] = sentenceSets.standardGeneralization(3,10,200,100)
+import numpy as np
+
+np.random.seed(1)
+
+#train_set,test_set = sentenceSets.standardGeneralization(3,10,200,100)
 #[train_set,test_set] = sentenceSets.spuriousAnticorrelation(3,10,200,100)
-[train_set,test_set] = sentenceSets.fullCombinatorial(3,10,200,100)
-print(train_set[1:10,])
-print(test_set[1:10,])
+#[train_set,test_set] = sentenceSets.fullCombinatorial(3,10,200,100)
+#print(train_set[0:9,])
+#print(train_set[-10:,])
+#print(test_set[0:9,])
+#print(test_set[-10:,])